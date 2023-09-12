acf27d5859648f4f4bad28dde7d610a38606eb43
katanaha1ban <109708405+katanaha1ban@users.noreply.github.com>
2023-09-13 07:04:56 +0900

    Add files via upload

--- mysterysolvinggame/script.js
+++ mysterysolvinggame/script.js
@@ -4,7 +4,7 @@ const submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', () => {
     const answer = answerInput.value;
 
-    if (answer === '1028') {
+    if (answer === '5026') {
         window.location.href = 'msg_getpoint.html'; // ダミーページへ遷移
     } else {
         alert('不正解です。');
